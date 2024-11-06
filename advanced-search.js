3de5f63977c47efc2374e4e3c92c60790a4e8123
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-06 09:36:50 +0600

    search result setting implemented

--- advanced-search.js
+++ advanced-search.js
@@ -1,96 +1,150 @@
-const S = async () => {
-    var c;
+const q = async () => {
+    var a;
     try {
       console.log("This is cdn");
-      const a =
-        (c = document.querySelector("html")) == null
+      const e =
+        (a = document.querySelector("html")) == null
           ? void 0
-          : c.getAttribute("data-wf-site");
-      if (!a) throw new Error("Site ID not found");
-      const n = await fetch(
-        `https://advanced-search-backend-production.up.railway.app/api/search/getSearchSettingsAndItems/${a}`
+          : a.getAttribute("data-wf-site");
+      if (!e) throw new Error("Site ID not found");
+      const m = await fetch(
+        `https://advanced-search-backend-production.up.railway.app/api/search/getSearchSettingsAndItems/${e}`
       );
-      if (!n.ok) throw new Error("Failed to fetch data");
-      const r = await n.json();
-      console.log("search settings", r.data), I(r);
-    } catch (a) {
-      console.error("Error fetching data:", a);
+      if (!m.ok) throw new Error("Failed to fetch data");
+      const n = await m.json();
+      console.log("search settings", n.data);
+      const s = await fetch(
+        `https://advanced-search-backend-production.up.railway.app/api/search/getSearchResultSettings/${e}`
+      );
+      if (!s.ok) throw new Error("Failed to fetch search result settings");
+      const p = (await s.json()).data;
+      L(n, p);
+    } catch (e) {
+      console.error("Error fetching data:", e);
     }
   },
-  I = (c) => {
-    const a = c.data.searchSettings;
-    c.data.items.results;
+  L = (a, e) => {
+    console.log("search result settings", e);
+    const m = a.data.searchSettings;
+    a.data.items.results, console.log("search settings", a.data);
     const n = document.getElementById("flowappz-asa-item-container"),
-      r = document.querySelector(".flowappz-asa-card");
-    if (!n || !r) {
+      s = document.querySelector(".flowappz-asa-card");
+    if (!n || !s) {
       console.error("Container or card template not found");
       return;
     }
-    (n.innerHTML = ""), (r.style.display = "none");
-    const h = document.getElementById("flowappz-asa-search-button"),
-      g = document.getElementById("flowappz-asa-search-input");
-    h.addEventListener("click", async (u) => {
-      var p;
-      u.preventDefault(), (n.innerHTML = "");
-      const w = g.value,
-        m =
-          (p = document.querySelector("html")) == null
-            ? void 0
-            : p.getAttribute("data-wf-site");
-      if (!m) return;
-      const y = `https://advanced-search-backend-production.up.railway.app/api/search/getSearchedItems/${m}?searchedItem=${encodeURIComponent(
-        w
-      )}`;
-      try {
-        const s = await fetch(y);
-        if (!s.ok) throw new Error(`Error: ${s.status}`);
-        const f = (await s.json()).data;
-        console.log("search results", f),
-          f.forEach((o) => {
-            const l = a.find((e) => e.itemCollection === o.index);
-            if (l)
-              if (l.dataField)
-                try {
-                  const e = JSON.parse(l.dataField),
-                    i = r.cloneNode(!0);
-                  if (((i.style.display = "flex"), e.title && o[e.title])) {
-                    const t = i.querySelector(
-                      '[flowappz-asa-data-field="title"]'
-                    );
-                    if (t) {
-                      const d = o[e.title].replace(/<\/?[^>]+(>|$)/g, "");
-                      t.innerText = d;
+    (n.innerHTML = ""), (s.style.display = "none"), (n.style.display = "none");
+    const h = document.getElementById("flowappz-asa-search-button");
+    h && !e.showSearchButton && (h.style.display = "none");
+    const p = document.getElementById("flowappz-asa-search-input"),
+      w = async () => {
+        var u;
+        n.innerHTML = "";
+        const y = p.value,
+          g =
+            (u = document.querySelector("html")) == null
+              ? void 0
+              : u.getAttribute("data-wf-site");
+        if (!g) return;
+        const I = `https://advanced-search-backend-production.up.railway.app/api/search/getSearchedItems/${g}?searchedItem=${encodeURIComponent(
+          y
+        )}`;
+        try {
+          const f = await fetch(I);
+          if (!f.ok) throw new Error(`Error: ${f.status}`);
+          const x = (await f.json()).data;
+          (n.style.display = "none"),
+            x.slice(0, e.itemLimit).forEach((r) => {
+              const i = m.find((o) => o.itemCollection === r.index);
+              if (i)
+                if (i.dataField)
+                  try {
+                    const o = JSON.parse(i.dataField),
+                      c = s.cloneNode(!0);
+                    if (
+                      ((n.style.display = "flex"),
+                      (c.style.display = "flex"),
+                      o.title && r[o.title])
+                    ) {
+                      const t = c.querySelector(
+                        '[flowappz-asa-data-field="title"]'
+                      );
+                      if (t) {
+                        const d = r[o.title].replace(/<\/?[^>]+(>|$)/g, "");
+                        t.innerText = d;
+                      }
                     }
-                  }
-                  if (e.description && o[e.description]) {
-                    const t = i.querySelector(
-                      '[flowappz-asa-data-field="description"]'
-                    );
-                    if (t) {
-                      const d = o[e.description].replace(/<\/?[^>]+(>|$)/g, "");
-                      t.innerText = d;
+                    if (
+                      i != null &&
+                      i.itemName &&
+                      e != null &&
+                      e.showCategory
+                    ) {
+                      const t = c.querySelector(
+                        '[flowappz-asa-data-field="category"]'
+                      );
+                      t && (t.innerText = i.itemName);
+                    } else {
+                      const t = c.querySelector(
+                        '[flowappz-asa-data-field="category"]'
+                      );
+                      t && (t.style.display = "none");
                     }
+                    if (o.description && r[o.description]) {
+                      const t = c.querySelector(
+                        '[flowappz-asa-data-field="description"]'
+                      );
+                      if (t) {
+                        const d = r[o.description].replace(
+                          /<\/?[^>]+(>|$)/g,
+                          ""
+                        );
+                        if (e != null && e.descriptionLength) {
+                          const T =
+                            d.length > e.descriptionLength
+                              ? d.slice(0, e.descriptionLength) + "..."
+                              : d;
+                          t.innerText = T;
+                        } else t.innerText = d;
+                      }
+                    }
+                    if (o.image && r[o.image]) {
+                      const t = c.querySelector(
+                        '[flowappz-asa-data-field="image"]'
+                      );
+                      t && ((t.src = r[o.image].url), (t.srcset = ""));
+                    }
+                    const E = c,
+                      l = r[o.link];
+                    l &&
+                      (typeof l == "string"
+                        ? (E.href = l)
+                        : typeof l == "object" && l.url && (E.href = l.url)),
+                      n.appendChild(c);
+                  } catch (o) {
+                    console.error("Error parsing fieldData:", o);
                   }
-                  if (e.image && o[e.image]) {
-                    const t = i.querySelector(
-                      '[flowappz-asa-data-field="image"]'
-                    );
-                    console.log("image url", o[e.image].url),
-                      console.log("image element", t),
-                      t && ((t.src = o[e.image].url), (t.srcset = ""));
-                  }
-                  n.appendChild(i);
-                } catch (e) {
-                  console.error("Error parsing fieldData:", e);
-                }
-              else
-                console.warn(
-                  `dataField is undefined or empty for itemCollection: ${l.itemCollection}`
-                );
-          });
-      } catch (s) {
-        console.error("Error fetching search results:", s);
-      }
+                else
+                  console.warn(
+                    `dataField is undefined or empty for itemCollection: ${i.itemCollection}`
+                  );
+            });
+        } catch (f) {
+          console.error("Error fetching search results:", f);
+        }
+      };
+    h.addEventListener("click", async (y) => {
+      y.preventDefault(), await w();
     });
+    const C = async () => {
+      console.log("search input changed"),
+        e != null &&
+          e.autoComplete &&
+          e != null &&
+          e.minCharacters &&
+          p.value.length >= e.minCharacters &&
+          (await w());
+    };
+    p.addEventListener("input", C);
   };
-S();
+q();
