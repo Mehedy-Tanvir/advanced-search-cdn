7bdfe49ca6cd6e0c6ef21360854e442ac16fa88d
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-13 10:19:53 +0600

    search button functionality fixed

--- advanced-search.js
+++ advanced-search.js
@@ -1,29 +1,31 @@
-const $ = async () => {
+const BaseUrl = "https://advanced-search-backend-production.up.railway.app";
+
+const L = async () => {
     var s;
     try {
-      // console.log("This is cdn");
+      console.log("This is cdn");
       const e =
         (s = document.querySelector("html")) == null
           ? void 0
           : s.getAttribute("data-wf-site");
       if (!e) throw new Error("Site ID not found");
       const h = await fetch(
-        `https://advanced-search-backend-production.up.railway.app/api/search/getSearchSettingsAndItems/${e}`
+        `${BaseUrl}/api/search/getSearchSettingsAndItems/${e}`
       );
       if (!h.ok) throw new Error("Failed to fetch data");
       const n = await h.json();
       console.log("search settings", n.data);
       const l = await fetch(
-        `https://advanced-search-backend-production.up.railway.app/api/search/getSearchResultSettings/${e}`
+        `${BaseUrl}/api/search/getSearchResultSettings/${e}`
       );
       if (!l.ok) throw new Error("Failed to fetch search result settings");
-      const f = (await l.json()).data;
-      b(n, f);
+      const m = (await l.json()).data;
+      $(n, m);
     } catch (e) {
       console.error("Error fetching data:", e);
     }
   },
-  b = (s, e) => {
+  $ = (s, e) => {
     console.log("search result settings", e);
     const h = s.data.searchSettings;
     s.data.items.results, console.log("search settings", s.data);
@@ -34,115 +36,115 @@ const $ = async () => {
       return;
     }
     (n.innerHTML = ""), (l.style.display = "none"), (n.style.display = "none");
-    const y = document.getElementById("flowappz-asa-search-button");
-    y && !e.showSearchButton && (y.style.display = "none");
-    const f = document.getElementById("flowappz-asa-search-input"),
+    const f = document.getElementById("flowappz-asa-search-button");
+    (f.type = "button"), f && !e.showSearchButton && (f.style.display = "none");
+    const m = document.getElementById("flowappz-asa-search-input"),
       E = async () => {
         var g;
         n.innerHTML = "";
-        const a = f.value,
+        const r = m.value,
           w =
             (g = document.querySelector("html")) == null
               ? void 0
               : g.getAttribute("data-wf-site");
         if (!w) return;
-        const u = `https://advanced-search-backend-production.up.railway.app/api/search/getSearchedItems/${w}?searchedItem=${encodeURIComponent(
-          a
+        const u = `${BaseUrl}/api/search/getSearchedItems/${w}?searchedItem=${encodeURIComponent(
+          r
         )}`;
         try {
-          const m = await fetch(u);
-          if (!m.ok) throw new Error(`Error: ${m.status}`);
-          const x = (await m.json()).data;
+          const y = await fetch(u);
+          if (!y.ok) throw new Error(`Error: ${y.status}`);
+          const x = (await y.json()).data;
           (n.style.display = "none"),
-            x.slice(0, e.itemLimit).forEach((r) => {
-              const i = h.find((o) => o.itemCollection === r.index);
-              if (i)
-                if (i.dataField)
+            x.slice(0, e.itemLimit).forEach((i) => {
+              const c = h.find((o) => o.itemCollection === i.index);
+              if (c)
+                if (c.dataField)
                   try {
-                    const o = JSON.parse(i.dataField),
-                      c = l.cloneNode(!0);
+                    const o = JSON.parse(c.dataField),
+                      a = l.cloneNode(!0);
                     if (
                       ((n.style.display = "flex"),
-                      (c.style.display = "flex"),
-                      o.title && r[o.title])
+                      (a.style.display = "flex"),
+                      o.title && i[o.title])
                     ) {
-                      const t = c.querySelector(
+                      const t = a.querySelector(
                         '[flowappz-asa-data-field="title"]'
                       );
                       if (t) {
-                        const p = r[o.title].replace(/<\/?[^>]+(>|$)/g, "");
+                        const p = i[o.title].replace(/<\/?[^>]+(>|$)/g, "");
                         t.innerText = p;
                       }
                     }
                     if (
-                      i != null &&
-                      i.itemName &&
+                      c != null &&
+                      c.itemName &&
                       e != null &&
                       e.showCategory
                     ) {
-                      const t = c.querySelector(
+                      const t = a.querySelector(
                         '[flowappz-asa-data-field="category"]'
                       );
-                      t && (t.innerText = i.itemName);
+                      t && (t.innerText = c.itemName);
                     } else {
-                      const t = c.querySelector(
+                      const t = a.querySelector(
                         '[flowappz-asa-data-field="category"]'
                       );
                       t && (t.style.display = "none");
                     }
-                    if (o.description && r[o.description]) {
-                      const t = c.querySelector(
+                    if (o.description && i[o.description]) {
+                      const t = a.querySelector(
                         '[flowappz-asa-data-field="description"]'
                       );
                       if (t) {
-                        const p = r[o.description].replace(
+                        const p = i[o.description].replace(
                           /<\/?[^>]+(>|$)/g,
                           ""
                         );
                         if (e != null && e.descriptionLength) {
-                          const L =
+                          const b =
                             p.length > e.descriptionLength
                               ? p.slice(0, e.descriptionLength) + "..."
                               : p;
-                          t.innerText = L;
+                          t.innerText = b;
                         } else t.innerText = p;
                       }
                     }
-                    if (o.image && r[o.image]) {
-                      const t = c.querySelector(
+                    if (o.image && i[o.image]) {
+                      const t = a.querySelector(
                         '[flowappz-asa-data-field="image"]'
                       );
-                      t && ((t.src = r[o.image].url), (t.srcset = ""));
+                      t && ((t.src = i[o.image].url), (t.srcset = ""));
                     }
-                    const I = c,
-                      d = r[o.link];
+                    const I = a,
+                      d = i[o.link];
                     d &&
                       (typeof d == "string"
                         ? (I.href = d)
                         : typeof d == "object" && d.url && (I.href = d.url)),
-                      n.appendChild(c);
+                      n.appendChild(a);
                   } catch (o) {
                     console.error("Error parsing fieldData:", o);
                   }
                 else
                   console.warn(
-                    `dataField is undefined or empty for itemCollection: ${i.itemCollection}`
+                    `dataField is undefined or empty for itemCollection: ${c.itemCollection}`
                   );
             });
-        } catch (m) {
-          console.error("Error fetching search results:", m);
+        } catch (y) {
+          console.error("Error fetching search results:", y);
         }
       };
-    function C(a, w) {
+    function C(r, w) {
       let u;
       return function (...g) {
-        clearTimeout(u), (u = setTimeout(() => a.apply(this, g), w));
+        clearTimeout(u), (u = setTimeout(() => r.apply(this, g), w));
       };
     }
-    y.addEventListener(
+    f.addEventListener(
       "click",
-      C(async (a) => {
-        a.preventDefault(), await E();
+      C(async (r) => {
+        r.preventDefault(), r.stopPropagation(), await E();
       }, 500)
     );
     const T = async () => {
@@ -151,14 +153,14 @@ const $ = async () => {
           e.autoComplete &&
           e != null &&
           e.minCharacters &&
-          f.value.length >= e.minCharacters &&
+          m.value.length >= e.minCharacters &&
           (await E());
     };
-    f.addEventListener(
+    m.addEventListener(
       "input",
-      C(async (a) => {
-        a.preventDefault(), await T();
+      C(async (r) => {
+        r.preventDefault(), await T();
       }, 500)
     );
   };
-$();
+L();
